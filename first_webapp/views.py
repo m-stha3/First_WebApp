6061f6c0d5730ca149a2d2017f564cf45c2293b2
Manish Shrestha <manaspunk@gmail.com>
2020-02-18 23:39:16 +0545

    My First Commit

---
+++ first_webapp/views.py
@@ -0,0 +1,15 @@
+
+from . import counter
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'index.html',{'key1':'i am coming from python code'})
+
+def result(request):
+    article = request.GET['article']
+    var_dict,word_count = counter.count(article)
+    return render(request, 'result.html',{'article':article,'word_count':word_count, 'dict_words':var_dict})
+
+"""def admin(request):
+    return render(request, "admin")"""
